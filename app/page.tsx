21663f194c8257657bb0092daf7fce1b1a90ea9d
Shubam-08 <shubamgupta165@gmail.com>
2025-07-03 17:59:50 +0530

    fix consultation changes

--- app/page.tsx
+++ app/page.tsx
@@ -206,7 +206,7 @@ export default function Home() {
 
 
       {/* Call to Action Section */}
-      <section className="py-20 bg-blue-600">
+      <section className="py-20 bg-[hsl(221,39%,11%)]">
         <div className="container mx-auto px-4">
           <div className="text-center">
             <h2 className="text-4xl font-bold text-white mb-6">
