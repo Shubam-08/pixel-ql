a3aa1c788330d1dec371270d3e1f083a8ade379f
Shubam-08 <shubamgupta165@gmail.com>
2025-07-09 16:39:00 +0530

    logo allignment change

--- components/HeaderLogo.tsx
+++ components/HeaderLogo.tsx
@@ -5,13 +5,13 @@ import Image from "next/image";
 
 export default function HeaderLogo() {
   return (
-    <div className="absolute top-2.5 left-4 z-50">
+    <div className="absolute top-6 sm:top-6.5 left-4 z-50">
       <Image
         src="/logonew1.png"
         alt="Company Logo"
         width={85}
         height={40}
-        className="object-contain"
+        className="object-contain w-[70px] sm:w-[85px] h-auto"
       />
     </div>
   );
